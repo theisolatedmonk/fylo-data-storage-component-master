d6d7bdb9221a8a66d4d08c811ee1f0b46aaa946a
theisolatedmonk <96rahulseth@gmail.com>
2023-09-05 23:52:37 +0530

    component folder created

---
+++ app/components/Cards.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Cards () {
+  return (
+    <div className='flex'>
+     <div className="flex flex-col">
+     <div className='flex'>
+        <Image  src={}/>
+      </div>
+      <div className=''>
+        <Image  src={}/>
+        <Image  src={}/>
+        <Image  src={}/>
+      </div>
+     </div>
+    </div>
+  )
+}
