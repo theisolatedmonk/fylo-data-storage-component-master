6e70cf5c7573adbe8a61d790e17dfb97e3c4cd8b
theisolatedmonk <96rahulseth@gmail.com>
2023-09-05 23:58:35 +0530

    icon imported

--- app/components/Cards.jsx
+++ app/components/Cards.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
 
+import folder from ' @/public/images/icon-folder.svg'
+import logo from ' @/public/images/icon-logo.svg'
+import upload from ' @/public/images/icon-upload.svg'
+import document from ' @/public/images/icon-document.svg'
+
 export default function Cards () {
   return (
     <div className='flex'>
